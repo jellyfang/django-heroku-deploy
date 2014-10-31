a7dc393f412fa91734a60c2bfbd570bff3d818a3
jw84 <jerrywang@me.com>
2014-10-30 22:08:18 -0700

    rebase

--- django_heroku/settings.py
+++ django_heroku/settings.py
@@ -62,7 +62,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    		'USER': '',
+				'PASSWORD': '',
+				'HOST': '',
+				'PORT': '',
+		}
 }
 
 # Internationalization
@@ -76,10 +80,10 @@ USE_TZ = True
 
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
+#DATABASES['default'] =  dj_database_url.config()
 
 # Enable Connection Pooling (if desired)
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
+#DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
